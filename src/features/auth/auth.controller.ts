8726932447395bfc4f914b64984fa4d75bee3183
Tomás Catena <tomascatena@gmail.com>
2022-08-13 20:25:37 +0200

    add husky

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -8,20 +8,22 @@ import httpStatus from 'http-status-codes';
  * @desc     Authenticate user & get token
  * @access   Private
  */
-const loginUser = catchAsync(async (req: RequestWithBody, res: Response) => {
-  res.status(httpStatus.OK).json({
-    message: 'Successfully logged in',
-  });
-});
+const loginUser = catchAsync(
+  async (req: RequestWithBody, res: Response) => {
+    res.status(httpStatus.OK).json({ message: 'Successfully logged in' });
+  },
+);
 
 /**
  * @route    GET api/v1/auth
  * @desc     Get auth user
  * @access   Private
  */
-const getUser = catchAsync(async (req: RequestWithBody, res: Response) => res.status(httpStatus.OK).json({
-  message: 'Successfully authenticated user',
-}));
+const getUser = catchAsync(
+  async (req: RequestWithBody, res: Response) => res.status(httpStatus.OK).json({
+    message: 'Successfully authenticated user',
+  }),
+);
 
 export default {
   loginUser,
